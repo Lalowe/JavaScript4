11757b8738ab59f59d87b36ebe9196675e39ad4f
Lalowe <vladimirpetr83@gmail.com>
2022-02-01 15:54:03 +0100

    santa come back

--- ex3.js
+++ ex3.js
@@ -1,5 +1,10 @@
 function removeTheSanta() {
-    this.style.opacity = "0";
+
+    if (this.style.opacity == 0) {
+        this.style.opacity = "1";
+    } else {
+        this.style.opacity = "0"
+    }
 }
 var santa = document.getElementsByClassName("btn")
 for (let i = 0; i < santa.length; i++) {
